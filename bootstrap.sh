eecf371186f00488cd1a4cc426936e410ae0d9ff
waterfountain1996 <r75930@gmail.com>
2022-12-10 18:57:09 +0200

    Update bootstrap script.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,22 +1,18 @@
 #!/usr/bin/env bash
 
-SSHOpts=("UsePAM" "X11Forwarding" "PasswordAuthentication" "PermitRootLogin")
+SSHOpts=("UsePAM" "UseDNS" "X11Forwarding" "PasswordAuthentication" "PermitRootLogin")
 
 for opt in "${SSHOpts[@]}"; do
 	sed -i "s/^#*$opt *[-A-Za-z0-9]*/$opt no/" /etc/ssh/sshd_config
 done
 
 apt update -y && apt upgrade -y
+apt install -y git tmux htop
 
 read -p "New user (blank to skip): "
 if [[ -n "$REPLY" ]]; then
-	useradd -G mail,sudo -m "$REPLY"
+	useradd -G mail,sudo -m -s /bin/bash "$REPLY"
 	passwd "$REPLY"
-	if [[ $? -ne 0 ]]; then
-		printf "Failed to set password, deleting user %s\n" "$REPLY"
-		userdel -r "$REPLY"
-	fi
-	chsh "$REPLY" -s /bin/bash
 fi
 
 systemctl reload sshd
